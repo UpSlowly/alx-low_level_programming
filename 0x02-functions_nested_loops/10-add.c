bed2ba5fce10bf6748b9e059c6b8088a68d70c33
CertainGoose <certaingoose@proton.me>
2023-03-22 11:39:55 +0300

    Betty

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,17 +1,13 @@
-/*
-Write a function that adds two integers and returns the result.
-
-    Prototype: int add(int, int);
-*/
-
 #include "main.h"
 
-int add(int,int)
+/**
+ * add - Adds two integers.
+ * @num1: The first integer to be added.
+ * @num2: The second integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+int add(int num1, int num2)
 {
-    int a = 5;
-    int b = 6;
-
-    int sum = a + b;
-
-    return (sum);
-};
+return (num1 + num2);
+}
