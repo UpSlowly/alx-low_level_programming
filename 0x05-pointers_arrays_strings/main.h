63ab9da08956f90620e5c65618021df9d5ccdae4
CertainGoose <certaingoose@proton.me>
2023-03-29 15:32:43 +0300

    Write a function that takes a pointer to an int as parameter

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
 *  reset_to_98 - takes the pointer and updates the value
 *  @n: int to check
@@ -6,7 +7,6 @@
 *  Return: 0 is success
 */
 
-void reset_to_98(int *n)
-{
-	*n = 98;
-};
+void reset_to_98(int *n) {
+  *n = 98;
+}
