bdcc917f52fb83f8a2577f15ee40dd7b8c1983ad
CertainGoose <certaingoose@proton.me>
2023-04-03 20:01:53 +0300

    betty

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,17 +7,22 @@
  * Return: number of bytes in the initial segment of s which consist only of
  * bytes from accept
  */
-unsigned int _strspn(char *s, char *accept) {
-    unsigned int i, j;
-    for (i = 0; s[i]; i++) {
-        for (j = 0; accept[j]; j++) {
-            if (s[i] == accept[j]) {
-                break;
-            }
-        }
-        if (!accept[j]) {
-            return i;
-        }
-    }
-    return i;
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, j;
+for (i = 0; s[i]; i++)
+{
+for (j = 0; accept[j]; j++)
+{
+if (s[i] == accept[j])
+{
+break;
+}
+}
+if (!accept[j])
+{
+return (i);
+}
+}
+return (i);
 }
