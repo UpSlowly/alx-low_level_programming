948f519289c74edbddc804aa5c3e189cd7ccb42a
CertainGoose <certaingoose@proton.me>
2023-04-03 20:14:00 +0300

    betty

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,21 +10,26 @@
  * substring is not found.
  */
 
-char *_strstr(char *haystack, char *needle) {
-    if (*needle == '\0') {
-        return haystack;
-    }
-    while (*haystack != '\0') {
-        char *h = haystack;
-        char *n = needle;
-        while (*n != '\0' && *h == *n) {
-            h++;
-            n++;
-        }
-        if (*n == '\0') {
-            return haystack;
-        }
-        haystack++;
-    }
-    return NULL;
+char *_strstr(char *haystack, char *needle)
+{
+if (*needle == '\0')
+{
+return (haystack);
+}
+while (*haystack != '\0')
+{
+char *h = haystack;
+char *n = needle;
+while (*n != '\0' && *h == *n)
+{
+h++;
+n++;
+}
+if (*n == '\0')
+{
+return (haystack);
+}
+haystack++;
+}
+return (NULL);
 }
