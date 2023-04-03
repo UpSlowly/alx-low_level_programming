2e6287c4fdf61c9bac87fc691374add91f2583e7
CertainGoose <certaingoose@proton.me>
2023-04-03 13:07:25 +0300

    helped

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,20 +8,27 @@
  */
 char *cap_string(char *str)
 {
-    int cap_next = 1; 
-    char *p = str;
-    
-    while (*p) {
-        if (isspace(*p) || ispunct(*p)) {
-            cap_next = 1; 
-        } else if (cap_next && islower(*p)) {
-            *p = toupper(*p); 
-            cap_next = 0; 
-        } else {
-            cap_next = 0;
-        }
-        p++;
-    }
-    
-    return str;
+	int conversion, index, count;
+
+	char chars[] = {' ', ',', ';', '.', '!',
+			 '?', '"', '(', ')', '{', '}',  '\t', '\n', '\0'};
+	conversion = 32;
+
+	for (index = 0; entry[index] != '\0'; index++)
+	{
+		if (entry[index] >= 'index' && entry[index] <= 'z')
+		{
+			entry[index] =  entry[index] - conversion;
+		}
+		conversion = 0;
+		for (count = 0; chars[count] != '\0'; count++)
+		{
+			if (chars[count] == entry[index])
+			{
+				conversion = 32;
+				break;
+			}
+		}
+	}
+	return (entry);
 }
