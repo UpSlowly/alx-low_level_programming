bdf61e1fdc2864c4ff77206d865d5f8024c90b0c
CertainGoose <certaingoose@proton.me>
2023-04-03 19:56:11 +0300

    Write a function that locates a character in a string.

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,15 +7,19 @@
  * Return: Return to the character found, if not return a NULL
  */
 
-char *_strchr(char *s, char c) {
-    while (*s != '\0') {
-        if (*s == c) {
-            return s;
-        }
-        s++;
-    }
-    if (c == '\0') {
-        return s;
-    }
-    return NULL;
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+if (c == '\0')
+{
+return (s);
+}
+return (NULL);
 }
