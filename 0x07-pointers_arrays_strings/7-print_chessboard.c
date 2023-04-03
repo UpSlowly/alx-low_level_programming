acde873c39614d7d485e67591634e851224e20b5
CertainGoose <certaingoose@proton.me>
2023-04-03 20:19:11 +0300

    betty

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,12 +5,15 @@
  * @a: This is my variable
  *
  */
-void print_chessboard(char (*a)[8]) {
-    int i, j;
-    for (i = 0; i < 8; i++) {
-        for (j = 0; j < 8; j++) {
-            printf("%c ", a[i][j]);
-        }
-        printf("\n");
-    }
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+printf("%c ", a[i][j]);
+}
+printf("\n");
+}
 }
