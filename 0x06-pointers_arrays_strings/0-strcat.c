0f7f02521d984724b4bbdf5e046f81351d102703
CertainGoose <certaingoose@proton.me>
2023-04-03 12:17:39 +0300

    desc

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+ *_strcat - Write a function that concatenates two strings.
+ *
+ *@dest: This is the output dest
+ *@src: This is the input source
+ *
+ * Return: This return to dest, that concatenates two strings
+ */
 char *_strcat(char *dest, char *src)
 {
     int dest_len = 0;
