a11aa6ac34f614bfb5b19de1256c56545ee10542
CertainGoose <certaingoose@proton.me>
2023-04-04 19:47:51 +0300

    Betty

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,30 +1,27 @@
-/*
-Write a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line.
-
-    You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
-    All your code should be in the main function
-    You can only use putchar three times in your code
-*/
-
 #include <stdio.h>
+
+/**
+* main - Letter from "a" to "z" and "z" to "a"
+* Return: 0 for success
+*/
 
 int main(void)
 {
-    char alf = 'a';
-    char ALF = 'A';
+char alf = 'a';
+char ALF = 'A';
 
-    while (alf <= 'z')
-    {
-        putchar(alf);
-        alf++;
-    }
+while (alf <= 'z')
+{
+putchar(alf);
+alf++;
+}
 
-    while (ALF <= 'Z')
-    {
-        putchar(ALF);
-        ALF++;
-    }
-    putchar('\n');
+while (ALF <= 'Z')
+{
+putchar(ALF);
+ALF++;
+}
+putchar('\n');
 
-    return (0);
+return (0);
 }
