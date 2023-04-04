fa3c485a31a8739a5f38edf0899aa04d29fc13c8
CertainGoose <certaingoose@proton.me>
2023-04-04 22:41:46 +0300

    Write a program that prints all possible different combinations of three digits.

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
+/**
+  * main - prints all possible different combinations of three digits
+  *
+  * Return: 0 if success
+**/
 int main(void)
 {
-    int diffcomb1, diffcomb2, diffcomb3;
-
-    for (diffcomb1 = 0; diffcomb1 < 8; diffcomb1++)
-    {
-        for (diffcomb2 = diffcomb1 + 1; diffcomb2 < 9; diffcomb2++)
-        {
-            for (diffcomb3 = diffcomb2 + 1; diffcomb3 < 10; diffcomb3++)
-            {
-                putchar((diffcomb1 % 10) + '0');
-                putchar((diffcomb2 % 10) + '0');
-                putchar((diffcomb3 % 10) + '0');
-
-                if (diffcomb1 == 7 && diffcomb2 == 8 && diffcomb3 == 9)
-                    continue;
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-
-    putchar('\n');
-
-    return (0);
+int diffcomb1, diffcomb2, diffcomb3;
+for (diffcomb1 = 0; diffcomb1 < 8; diffcomb1++)
+{
+for (diffcomb2 = diffcomb1 + 1; diffcomb2 < 9; diffcomb2++)
+{
+for (diffcomb3 = diffcomb2 + 1; diffcomb3 < 10; diffcomb3++)
+{
+putchar((diffcomb1 % 10) + '0');
+putchar((diffcomb2 % 10) + '0');
+putchar((diffcomb3 % 10) + '0');
+if (diffcomb1 == 7 && diffcomb2 == 8 && diffcomb3 == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
