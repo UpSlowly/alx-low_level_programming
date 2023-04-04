5b53cf37fe54fdcdcfd31346e63af5d13c0bf41e
CertainGoose <certaingoose@proton.me>
2023-04-04 20:32:38 +0300

    Write a program that prints the lowercase alphabet in reverse, followed by a new line.

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,23 +1,17 @@
-/*
-Write a program that prints the lowercase alphabet in reverse, followed by a new line.
-
-    You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
-    All your code should be in the main function
-    You can only use putchar twice in your code
-*/
-
 #include <stdio.h>
-
+/**
+* main - Print alphabet backwards
+*
+* Return: 0 for success
+*/
 int main(void)
 {
-    char alf = 'z';
-    while (alf >= 'a')
-    {
-        /* code */
-        putchar(alf);
-        alf--;
-    }
-    putchar('\n');
-
-    return 0;
+char alf = 'z';
+while (alf >= 'a')
+{
+putchar(alf);
+alf--;
+}
+putchar('\n');
+return (0);
 }
