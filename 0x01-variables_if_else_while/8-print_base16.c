49e08265b06927d99af1603b738386d0fbf4121a
CertainGoose <certaingoose@proton.me>
2023-04-04 20:49:34 +0300

    Write a program that prints all the numbers of base 16 in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,27 +1,19 @@
-/*
-Write a program that prints all the numbers of base 16 in lowercase, followed by a new line.
-
-    You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
-    All your code should be in the main function
-    You can only use putchar three times in your code
-*/
-
 #include <stdio.h>
+
+/**
+* main - Print Numbers Hexadecimal
+*
+* Return: 0 for success
+*
+**/
 
 int main(void)
 {
-    int singnum = 24;
-
-    while (singnum <= 102)
-    {
-        putchar(singnum);
-        
-//Search on this meaning
-        if (singnum == 57)  
-            singnum += 39;
-        singnum++;
-    }
-    putchar('\n');
-    putchar('\n');
-    return 0;
+for (int i = 0; i < 16; i++)
+{
+int c = i < 10 ? i + '0' : i - 10 + 'a';
+putchar(c);
+}
+putchar('\n');
+return (0);
 }
